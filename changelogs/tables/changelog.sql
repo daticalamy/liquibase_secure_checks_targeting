fed0004939b8c5e108ac71a65cc6148e744553f5
Amy Smith <asmith@liquibase.com>
2026-08-14 12:05:54 -0500

    restrict/exempt

--- changelogs/tables/changelog.sql
+++ changelogs/tables/changelog.sql
@@ -8,3 +8,17 @@ CREATE TABLE my_table (
 
 --changeset asmith:grant-001
 GRANT USAGE ON SCHEMA public TO postgres;
+
+--changeset asmith:createTable-0001
+CREATE TABLE my_table0001 (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+--changeset amy.smith labels:CDW-462736 contextFilter:PRD
+insert into my_table0001 (name)
+values ('Amy Smith');
+
+--changeset amy.smith:truncate_001
+truncate table my_table0001;
+
